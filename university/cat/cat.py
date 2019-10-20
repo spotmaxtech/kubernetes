519df62dd83e062fe7da7f1d0a2e1968f6a34118
Liuzoxan <liuzoxan@gmail.com>
2019-10-20 20:49:29 +0800

    update cat

--- university/cat/cat.py
+++ university/cat/cat.py
@@ -1,11 +1,33 @@
-import SimpleHTTPServer
-import SocketServer
+import socket
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-PORT = 8000
+PORT = 5000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+class myHandler(BaseHTTPRequestHandler):
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+    # Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
 
-print "serving at port", PORT
-httpd.serve_forever()
+        self.wfile.write("Hello World! I'm little cat!\n")
+        self.wfile.write("Hostname: " + hostname + "\n")
+        self.wfile.write("Ip:       " + ip)
+        return
+
+if __name__ == "__main__":
+    try:
+        server = HTTPServer(('', PORT), myHandler)
+        print 'Started httpserver on port ' , PORT
+
+        # Wait forever for incoming htto requests
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print '^C received, shutting down the web server'
+        server.socket.close()
+
