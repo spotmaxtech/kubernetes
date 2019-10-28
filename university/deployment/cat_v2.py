757d36ec0721dc3027398b7d217356ce08efb088
Liuzoxan <liuzoxan@gmail.com>
2019-10-28 10:56:29 +0800

    update

---
+++ university/deployment/cat_v2.py
@@ -0,0 +1,32 @@
+import socket
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+PORT = 5000
+
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    # Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send the html message
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+
+        self.wfile.write("Hello World! I'm little cat!\n")
+        self.wfile.write("Hostname: " + hostname + "\n")
+        self.wfile.write("Ip:       " + ip + "\n")
+        self.wfile.write("Version:  v2\n")
+        return
+
+
+if __name__ == "__main__":
+    server = HTTPServer(('', PORT), MyHandler)
+    print 'Started http server on port ', PORT
+
+    # Wait forever for incoming http requests
+    server.serve_forever()
+
